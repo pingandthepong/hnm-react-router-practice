bec921c89ec0b378b57d75179c04d5d31ebd5866
pingandthepong <pingandthepong@gmail.com>
2025-10-28 17:35:49 +0900

    Routes 및 base 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import ProductAll from "./page/ProductAll";
+import Login from "./page/Login";
+import ProductDetail from "./page/ProductDetail";
 
-// 1. 전체페이지, 로그인페이지, 디테일페이지
-// 2. Route 구성
-// 3. 검색
+// * Goals *
+// 1. Router 구성
+// 2. 검색
+
+// * User Story *
+// TODO: 1. 전체상품페이지, 로그인페이지, 상품디테일페이지
+// TODO: 2. 전체상품페이지: 전체 상품을 볼 수 있다.
+// TODO: 3. 로그인페이지: 로그인 페이지가 나옴
+// TODO: 4. 상품디테일 클릭 시, isLogin ? 상품디테일페이지 : 로그인페이지
+// TODO: 5. 로그아웃버튼 클릭 시, 로그아웃 & 상품디테일 페이지 볼 수 없음 & 다시 로그인페이지 보임
+// TODO: 6. 로그인을 하면 로그아웃이 보이고, 로그아웃을 하면 로그인이 보인다.
+// TODO: 7. 상품 검색 가능
 
 function App() {
-  return <>test</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
