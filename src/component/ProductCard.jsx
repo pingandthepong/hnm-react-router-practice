798e5e157c399f9be3682ec4112ef9e9cd27dd4b
pingandthepong <pingandthepong@gmail.com>
2025-10-29 22:56:37 +0900

    [style] ProductCard 스타일링

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Badge from "react-bootstrap/Badge";
+import Stack from "react-bootstrap/Stack";
 
 const ProductCard = ({ item }) => {
   const price = new Intl.NumberFormat("ko-KR").format(item?.price);
@@ -10,15 +12,33 @@ const ProductCard = ({ item }) => {
   };
 
   return (
-    <div onClick={showDetail}>
+    <Stack onClick={showDetail} gap={1}>
       <div className="image-wrapper">
-        <img width={"100%"} src={item?.img} alt="" />
+        <img width={"100%"} src={item?.img} alt={`${item?.title} 이미지`} />
       </div>
-      <div>{item?.choice ? "Conscious Choice" : ""}</div>
-      <div>{item?.title}</div>
-      <div>₩{price}</div>
-      <div>{item?.new ? "NEW" : ""}</div>
-    </div>
+      <Stack direction="horizontal" gap={1}>
+        <div>
+          {item?.choice ? (
+            <Badge pill bg="primary">
+              Conscious Choice
+            </Badge>
+          ) : (
+            ""
+          )}
+        </div>
+        <div>
+          {item?.new ? (
+            <Badge pill bg="warning">
+              New
+            </Badge>
+          ) : (
+            ""
+          )}
+        </div>
+      </Stack>
+      <div style={{ fontSize: ".85rem", fontWeight: 700 }}>{item?.title}</div>
+      <div style={{ fontSize: ".75rem", fontWeight: 300 }}>₩{price}</div>
+    </Stack>
   );
 };
 
