35f155366649491b9417de8f6de1f095d03ce7b2
pingandthepong <pingandthepong@gmail.com>
2025-10-31 00:37:34 +0900

    [feat] 로그인 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import PrivateRoute from "./route/PrivateRoute";
 
 function App() {
   let [loading, setLoading] = useState(false);
-  let [authenticate, setAuthenticate] = useState(true); // false 비로그인, true 로그인
+  let [authenticate, setAuthenticate] = useState(false); // false 비로그인, true 로그인
   const myUrl =
     "https://my-json-server.typicode.com/pingandthepong/hnm-react-router-practice/";
   // const myUrl = " http://localhost:3000/";
